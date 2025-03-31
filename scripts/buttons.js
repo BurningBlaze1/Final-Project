8f7524abb93e05a804f480afaa6a4ba1546fd57f
BurningBlaze1 <kenn0559@algonquinlive.com>
2025-03-31 13:51:25 -0400

    curtains

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -1,8 +1,15 @@
-// const element = document.getElementById("koncarousel_wrapper");
-
 
 
 function ToggleOutfit1() {
+    curtain.classList.add('curtainsTransition')
+    curtain2.classList.add('curtainsTransition2')
+
+    button1.classList.add('disabled')
+    button2.classList.add('disabled')
+    button3.classList.add('disabled')
+    button4.classList.add('disabled')
+
+    setTimeout(()=> {
     if (document.body.classList.contains('button1')) {
 
     } 
@@ -12,83 +19,115 @@ function ToggleOutfit1() {
         koncarouselimg2.classList.add('hidden')
         koncarouselimg3.classList.add('hidden')
         koncarouselimg4.classList.add('hidden')
-        // koncarousel_wrappper.classlist.add('curtainsTransition')
     }
+}, 3000)
+
+setTimeout(()=> {
+    curtain.classList.remove('curtainsTransition')
+    curtain2.classList.remove('curtainsTransition2')
+
+    // button1.classList.remove('disabled')
+    button2.classList.remove('disabled')
+    button3.classList.remove('disabled')
+    button4.classList.remove('disabled')
+}, 6000)
+
 }
 
 function ToggleOutfit2() {
-    // document.body.classList.toggle('#carouselimg3test')
+ curtain.classList.add('curtainsTransition')
+    curtain2.classList.add('curtainsTransition2')
 
-    if (document.body.classList.contains('button2')) {
-        // document.getElementById('btnoutfit2').innerText = 'off'
-    } else {
-        // document.getElementById('btnoutfit2').innerText = 'on'
+    button1.classList.add('disabled')
+    button2.classList.add('disabled')
+    button3.classList.add('disabled')
+    button4.classList.add('disabled')
+
+    setTimeout(()=> {
+    if (document.body.classList.contains('button2'))
+        {
+
+    } 
+
+    else {
         koncarouselimg1.classList.add('hidden')
         koncarouselimg2.classList.remove('hidden')
         koncarouselimg3.classList.add('hidden')
         koncarouselimg4.classList.add('hidden')
-        //  document.getElementById('btnoutfit1').innerText = 'off'
-        //  document.getElementById('btnoutfit3').innerText = 'off'
-        //  document.getElementById('btnoutfit4').innerText = 'off'
     }
+}, 3000)
+
+setTimeout(()=> {
+    curtain.classList.remove('curtainsTransition')
+    curtain2.classList.remove('curtainsTransition2')
+    
+    button1.classList.remove('disabled')
+    // button2.classList.remove('disabled')
+    button3.classList.remove('disabled')
+    button4.classList.remove('disabled')
+}, 6000)
+
 }
 
 function ToggleOutfit3() {
-    // document.body.classList.toggle('#carouselimg3test')
+ curtain.classList.add('curtainsTransition')
+    curtain2.classList.add('curtainsTransition2')
 
+    button1.classList.add('disabled')
+    button2.classList.add('disabled')
+    button3.classList.add('disabled')
+    button4.classList.add('disabled')
+
+    setTimeout(()=> {
     if (document.body.classList.contains('button3')) {
-        // document.getElementById('btnoutfit3').innerText = 'off'
-    } else {
-        // document.getElementById('btnoutfit3').innerText = 'on'
+    } 
+    else {
         koncarouselimg1.classList.add('hidden')
         koncarouselimg2.classList.add('hidden')
         koncarouselimg3.classList.remove('hidden')
         koncarouselimg4.classList.add('hidden')
-        //  document.getElementById('btnoutfit1').innerText = 'off'
-        //  document.getElementById('btnoutfit2').innerText = 'off'
-        //  document.getElementById('btnoutfit4').innerText = 'off'
     }
+}, 3000)
+
+setTimeout(()=> {
+    curtain.classList.remove('curtainsTransition')
+    curtain2.classList.remove('curtainsTransition2')
+
+    button1.classList.remove('disabled')
+    button2.classList.remove('disabled')
+    // button3.classList.remove('disabled')
+    button4.classList.remove('disabled')
+}, 6000)
+
 }
 
 function ToggleOutfit4() {
-    // document.body.classList.toggle('#carouselimg3test')
+    curtain.classList.add('curtainsTransition')
+    curtain2.classList.add('curtainsTransition2')
 
+    button1.classList.add('disabled')
+    button2.classList.add('disabled')
+    button3.classList.add('disabled')
+    button4.classList.add('disabled')
+
+    setTimeout(()=> {
     if (document.body.classList.contains('button4')) {
-        // document.getElementById('btnoutfit4').innerText = 'off'
     } else {
-        // document.getElementById('btnoutfit4').innerText = 'on'
         koncarouselimg1.classList.add('hidden')
         koncarouselimg2.classList.add('hidden')
         koncarouselimg3.classList.add('hidden')
         koncarouselimg4.classList.remove('hidden')
-        //  document.getElementById('btnoutfit1').innerText = 'off'
-        //  document.getElementById('btnoutfit2').innerText = 'off'
-        //  document.getElementById('btnoutfit3').innerText = 'off'
-
-
     }
+}, 3000)
+
+setTimeout(()=> {
+    curtain.classList.remove('curtainsTransition')
+    curtain2.classList.remove('curtainsTransition2')
+
+    button1.classList.remove('disabled')
+    button2.classList.remove('disabled')
+    button3.classList.remove('disabled')
+    // button4.classList.remove('disabled')
+}, 6000)
+
 }
-
-// let $carouse3 = document.getElementById('carousel3')
-
-// let index3 = 0
-// setInterval(() => {
-//     let $images3 = $carousel3.children
-    
-//     index++
-//     if (index >= $images3.length) {
-//         index = 0
-//     }
-
-//     for (let i = 0; i < $images3.length; i++) {
-//         if (i == index) {
-//             $images3[i].classList.remove('hidden')
-//         } else {
-//             $images3[i].classList.add('hidden')
-//         }
-//     }
-
-//     // for (let $image of $images) {
-//     //     console.log($image)
-//     // }
-// }, )
