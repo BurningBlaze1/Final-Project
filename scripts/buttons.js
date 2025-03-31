85bdf08d97252da62a7578dfcbf3d0cfbf73e2a8
BurningBlaze1 <kenn0559@algonquinlive.com>
2025-03-31 17:09:44 -0400

    edits

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -9,33 +9,33 @@ function ToggleOutfit1() {
     button3.classList.add('disabled')
     button4.classList.add('disabled')
 
-    setTimeout(()=> {
-    if (document.body.classList.contains('button1')) {
+    setTimeout(() => {
+        if (document.body.classList.contains('button1')) {
 
-    } 
+        }
 
-    else {
-        koncarouselimg1.classList.remove('hidden')
-        koncarouselimg2.classList.add('hidden')
-        koncarouselimg3.classList.add('hidden')
-        koncarouselimg4.classList.add('hidden')
-    }
-}, 3000)
+        else {
+            koncarouselimg1.classList.remove('hidden')
+            koncarouselimg2.classList.add('hidden')
+            koncarouselimg3.classList.add('hidden')
+            koncarouselimg4.classList.add('hidden')
+        }
+    }, 3000)
 
-setTimeout(()=> {
-    curtain.classList.remove('curtainsTransition')
-    curtain2.classList.remove('curtainsTransition2')
+    setTimeout(() => {
+        curtain.classList.remove('curtainsTransition')
+        curtain2.classList.remove('curtainsTransition2')
 
-    // button1.classList.remove('disabled')
-    button2.classList.remove('disabled')
-    button3.classList.remove('disabled')
-    button4.classList.remove('disabled')
-}, 6000)
+        // button1.classList.remove('disabled')
+        button2.classList.remove('disabled')
+        button3.classList.remove('disabled')
+        button4.classList.remove('disabled')
+    }, 6000)
 
 }
 
 function ToggleOutfit2() {
- curtain.classList.add('curtainsTransition')
+    curtain.classList.add('curtainsTransition')
     curtain2.classList.add('curtainsTransition2')
 
     button1.classList.add('disabled')
@@ -43,34 +43,33 @@ function ToggleOutfit2() {
     button3.classList.add('disabled')
     button4.classList.add('disabled')
 
-    setTimeout(()=> {
-    if (document.body.classList.contains('button2'))
-        {
+    setTimeout(() => {
+        if (document.body.classList.contains('button2')) {
 
-    } 
+        }
 
-    else {
-        koncarouselimg1.classList.add('hidden')
-        koncarouselimg2.classList.remove('hidden')
-        koncarouselimg3.classList.add('hidden')
-        koncarouselimg4.classList.add('hidden')
-    }
-}, 3000)
+        else {
+            koncarouselimg1.classList.add('hidden')
+            koncarouselimg2.classList.remove('hidden')
+            koncarouselimg3.classList.add('hidden')
+            koncarouselimg4.classList.add('hidden')
+        }
+    }, 3000)
 
-setTimeout(()=> {
-    curtain.classList.remove('curtainsTransition')
-    curtain2.classList.remove('curtainsTransition2')
-    
-    button1.classList.remove('disabled')
-    // button2.classList.remove('disabled')
-    button3.classList.remove('disabled')
-    button4.classList.remove('disabled')
-}, 6000)
+    setTimeout(() => {
+        curtain.classList.remove('curtainsTransition')
+        curtain2.classList.remove('curtainsTransition2')
+
+        button1.classList.remove('disabled')
+        // button2.classList.remove('disabled')
+        button3.classList.remove('disabled')
+        button4.classList.remove('disabled')
+    }, 6000)
 
 }
 
 function ToggleOutfit3() {
- curtain.classList.add('curtainsTransition')
+    curtain.classList.add('curtainsTransition')
     curtain2.classList.add('curtainsTransition2')
 
     button1.classList.add('disabled')
@@ -78,26 +77,26 @@ function ToggleOutfit3() {
     button3.classList.add('disabled')
     button4.classList.add('disabled')
 
-    setTimeout(()=> {
-    if (document.body.classList.contains('button3')) {
-    } 
-    else {
-        koncarouselimg1.classList.add('hidden')
-        koncarouselimg2.classList.add('hidden')
-        koncarouselimg3.classList.remove('hidden')
-        koncarouselimg4.classList.add('hidden')
-    }
-}, 3000)
+    setTimeout(() => {
+        if (document.body.classList.contains('button3')) {
+        }
+        else {
+            koncarouselimg1.classList.add('hidden')
+            koncarouselimg2.classList.add('hidden')
+            koncarouselimg3.classList.remove('hidden')
+            koncarouselimg4.classList.add('hidden')
+        }
+    }, 3000)
 
-setTimeout(()=> {
-    curtain.classList.remove('curtainsTransition')
-    curtain2.classList.remove('curtainsTransition2')
+    setTimeout(() => {
+        curtain.classList.remove('curtainsTransition')
+        curtain2.classList.remove('curtainsTransition2')
 
-    button1.classList.remove('disabled')
-    button2.classList.remove('disabled')
-    // button3.classList.remove('disabled')
-    button4.classList.remove('disabled')
-}, 6000)
+        button1.classList.remove('disabled')
+        button2.classList.remove('disabled')
+        // button3.classList.remove('disabled')
+        button4.classList.remove('disabled')
+    }, 6000)
 
 }
 
@@ -110,24 +109,24 @@ function ToggleOutfit4() {
     button3.classList.add('disabled')
     button4.classList.add('disabled')
 
-    setTimeout(()=> {
-    if (document.body.classList.contains('button4')) {
-    } else {
-        koncarouselimg1.classList.add('hidden')
-        koncarouselimg2.classList.add('hidden')
-        koncarouselimg3.classList.add('hidden')
-        koncarouselimg4.classList.remove('hidden')
-    }
-}, 3000)
+    setTimeout(() => {
+        if (document.body.classList.contains('button4')) {
+        } else {
+            koncarouselimg1.classList.add('hidden')
+            koncarouselimg2.classList.add('hidden')
+            koncarouselimg3.classList.add('hidden')
+            koncarouselimg4.classList.remove('hidden')
+        }
+    }, 3000)
 
-setTimeout(()=> {
-    curtain.classList.remove('curtainsTransition')
-    curtain2.classList.remove('curtainsTransition2')
+    setTimeout(() => {
+        curtain.classList.remove('curtainsTransition')
+        curtain2.classList.remove('curtainsTransition2')
 
-    button1.classList.remove('disabled')
-    button2.classList.remove('disabled')
-    button3.classList.remove('disabled')
-    // button4.classList.remove('disabled')
-}, 6000)
+        button1.classList.remove('disabled')
+        button2.classList.remove('disabled')
+        button3.classList.remove('disabled')
+        // button4.classList.remove('disabled')
+    }, 6000)
 
 }
